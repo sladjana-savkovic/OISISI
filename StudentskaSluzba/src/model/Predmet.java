b3e4e74b5dd25d45f0b7dc408b4119dcc4473689
Sladjana <sladjasavkovic333@gmail.com>
2019-12-01 23:30:56 +0100

    #predmet
    Pocetna verzija klase Predmet

---
+++ StudentskaSluzba/src/model/Predmet.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * @author Sladjana Savkovic
+ *
+ */
+public class Predmet {
+	private String sifra,naziv,predmetniProfesor;
+	private int semestar,godinaStudija;
+	private ArrayList<String> spisakStudenata=new ArrayList<String>();
+	
+	public Predmet(String sifra, String naziv, String predmetniProfesor, int semestar, int godinaStudija,
+			ArrayList<String> spisakStudenata) {
+		super();
+		this.sifra = sifra;
+		this.naziv = naziv;
+		this.predmetniProfesor = predmetniProfesor;
+		this.semestar = semestar;
+		this.godinaStudija = godinaStudija;
+		this.spisakStudenata = spisakStudenata;
+	}
+
+	public String getSifra() {
+		return sifra;
+	}
+
+	public void setSifra(String sifra) {
+		this.sifra = sifra;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public String getPredmetniProfesor() {
+		return predmetniProfesor;
+	}
+
+	public void setPredmetniProfesor(String predmetniProfesor) {
+		this.predmetniProfesor = predmetniProfesor;
+	}
+
+	public int getSemestar() {
+		return semestar;
+	}
+
+	public void setSemestar(int semestar) {
+		this.semestar = semestar;
+	}
+
+	public int getGodinaStudija() {
+		return godinaStudija;
+	}
+
+	public void setGodinaStudija(int godinaStudija) {
+		this.godinaStudija = godinaStudija;
+	}
+
+	public ArrayList<String> getSpisakStudenata() {
+		return spisakStudenata;
+	}
+
+	public void setSpisakStudenata(ArrayList<String> spisakStudenata) {
+		this.spisakStudenata = spisakStudenata;
+	}
+}
