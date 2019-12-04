b049671612b14ceca7a2de28e664fd7e0280e219
Sladjana <sladjasavkovic333@gmail.com>
2019-12-05 00:09:24 +0100

    #MainFrame
    Ubacene nove funkcionalnosti u glavni prozor: Toolbar, TabbedPane.

--- StudentskaSluzba/src/view/MainFrame.java
+++ StudentskaSluzba/src/view/MainFrame.java
@@ -13,6 +13,10 @@ import javax.swing.*;
  */
 public class MainFrame extends JFrame{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private static MainFrame instance = null;
 
 	public static MainFrame getInstance() {
@@ -29,16 +33,30 @@ public class MainFrame extends JFrame{
 		setSize(screenSize.width * 3 / 4, screenSize.height * 3 / 4);
 		//postavljamo JFrame na centar ekrana
 		setLocationRelativeTo(null);
-		setVisible(true);
 		 //Postavljamo akciju pri zatvaranju prozora
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		//Podesavanje naslova
-		setTitle("Studentska sluzba");
+		setTitle("Studentska sluzba");	
 		//Podesavanje boje pozadine
 		Color lightBlue= new Color(221,245,250);
 		getContentPane().setBackground(lightBlue);
 		//Dodavanje ikonice
 		Image img=kit.getImage("logo_images/ftn.png");
 		setIconImage(img);
+		
+		createToolbar();
+		createTabbedPane();
+		
+		setVisible(true);
+	}
+	public void createToolbar() {
+		Toolbar toolbar = new Toolbar();
+		// dodajem Toolbar u moj Frame, klasa BorderLayout se odnosi na rad sa prostorim rasporedom
+		//Toolbar postavljam na vrh glavne forme
+		add(toolbar, BorderLayout.NORTH);
+	}
+	public void createTabbedPane() {
+		TabbedPane tabbedPane = new TabbedPane();
+		add(tabbedPane,BorderLayout.CENTER);
 	}
 }
