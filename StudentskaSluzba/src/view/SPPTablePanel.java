e4438bef711970480225833d9cee08498c6b6075
Sladjana <sladjasavkovic333@gmail.com>
2019-12-15 23:48:17 +0100

    #SPPTablePanel
    Panel koji sluzi za impelentaciju tabela studenata,profesora i predmeta

---
+++ StudentskaSluzba/src/view/SPPTablePanel.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+/**
+ * @author Sladjana Savkovic
+ *
+ */
+public class SPPTablePanel extends JPanel{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel leftPanel = new JPanel();
+	//private Component c;
+	private JPanel rightPanel = new JPanel();
+	
+	
+	public SPPTablePanel() {
+		Color lightBlue= new Color(160,215,255);
+		setLayout(new BorderLayout());
+		add(leftPanel, BorderLayout.WEST);
+		leftPanel.setBackground(lightBlue);
+		add(rightPanel, BorderLayout.EAST);
+		rightPanel.setBackground(lightBlue);
+	}
+	
+	public SPPTablePanel(Component c, int leftGap, int rightGap) {
+		this();
+		leftPanel.setPreferredSize(new Dimension(leftGap, leftGap));
+		rightPanel.setPreferredSize(new Dimension(leftGap, leftGap));
+		
+		add(c, BorderLayout.CENTER);
+		
+		
+	}
+
+	@Override
+	public void setBackground(Color bg) {
+		// TODO Auto-generated method stub
+		super.setBackground(bg);
+		if (leftPanel == null || rightPanel == null) 
+			return;
+		leftPanel.setBackground(bg);
+		rightPanel.setBackground(bg);
+	}
+}
