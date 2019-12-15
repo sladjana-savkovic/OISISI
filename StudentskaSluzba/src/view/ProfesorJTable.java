1dec8b1c46d585eaa03c3eecf4b3b1b619b636ee
Sladjana <sladjasavkovic333@gmail.com>
2019-12-15 23:45:53 +0100

    #ProfesorJTable
    Tabela profesora.

---
+++ StudentskaSluzba/src/view/ProfesorJTable.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package view;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.TableCellRenderer;
+
+/**
+ * @author Sladjana Savkovic
+ *
+ */
+public class ProfesorJTable extends JTable{
+	
+	public ProfesorJTable() {
+		this.setRowSelectionAllowed(true);
+		this.setColumnSelectionAllowed(true);
+		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		this.setModel(new AbstractTableModelProfesor());
+		new ButtonColumnProfesor(this, 11);
+	}
+	@Override
+	public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+		Color lightBlue= new Color(160,215,255);
+		Component c = super.prepareRenderer(renderer, row, column);
+		// selektovani red ce imati drugaciju boju od ostalih
+		if (isRowSelected(row)) {
+			c.setBackground(lightBlue);
+		} else {
+			c.setBackground(Color.WHITE);
+		}
+		return c;
+	}
+}
