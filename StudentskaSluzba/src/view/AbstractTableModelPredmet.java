aa3e8c9f4917c12b55b421fd11f3eab4834a2ae1
Sladjana <sladjasavkovic333@gmail.com>
2019-12-15 23:44:37 +0100

    #AbstractTableModelPredmet
    Apstraktna klasa koja ce misluziti za kreiranje tabele svih predmeta.

---
+++ StudentskaSluzba/src/view/AbstractTableModelPredmet.java
@@ -0,0 +1,70 @@
+package view;
+
+import java.util.ArrayList;
+
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+
+import model.BazaPredmeta;
+import model.Predmet;
+
+/**
+ * @author Sladjana Savkovic
+ *
+ */
+
+public class AbstractTableModelPredmet extends AbstractTableModel{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String[] columnNames = { "Šifra", "Naziv", "Godina",
+			"Semestar", "Profesor", "Broj studenata", "Studenti" };
+	
+	public AbstractTableModelPredmet() {}
+	
+	@Override
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		return BazaPredmeta.getInstance().getPredmeti().size();
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+			if (columnIndex < 6)
+				return BazaPredmeta.getInstance().getValueAt(rowIndex, columnIndex);
+			else if (columnIndex == 6) {
+				JButton btn = new JButton("" + rowIndex);
+				return btn;
+			}
+			return null;
+	}
+
+	// da bismo mogli prikazati dugme
+		@Override
+		public Class<?> getColumnClass(int columnIndex) {
+			switch (columnIndex) {
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+				return JButton.class;
+			default:
+				return null;
+			}
+		}
+		@Override
+		public String getColumnName(int col) {
+			return columnNames[col];
+		}
+		@Override
+		public boolean isCellEditable(int rowIndex, int columnIndex) {
+			return columnIndex == 6;
+		}
+}
