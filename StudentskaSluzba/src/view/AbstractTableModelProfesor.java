8610f328053c151e10c567f4a71bc7c64a57f10c
Sladjana <sladjasavkovic333@gmail.com>
2019-12-15 23:45:06 +0100

    #AbstractTableProfesor
    Apstraktna klasa koja ce mi sluziti za kreiranje tabele svih profesora.

---
+++ StudentskaSluzba/src/view/AbstractTableModelProfesor.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package view;
+
+import javax.swing.JButton;
+import javax.swing.table.AbstractTableModel;
+
+import model.BazaPredmeta;
+import model.BazaProfesora;
+
+/**
+ * @author Sladjana Savkovic
+ *
+ */
+public class AbstractTableModelProfesor extends AbstractTableModel{
+	
+	private static final long serialVersionUID = 1L;
+	private String[] columnNames = {"Ime","Prezime","Datum rođenja","Adresa stanovanja","Kontakt telefon","E-mail adresa",
+			"Adresa kancelarije","Broj lične karte","Titula","Zvanje","Broj predmeta","Predmeti"};
+	
+	public AbstractTableModelProfesor() {}
+
+	@Override
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		return BazaProfesora.getInstance().getProfesori().size();
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		if (columnIndex < 11)
+			return BazaProfesora.getInstance().getValueAt(rowIndex, columnIndex);
+		else if (columnIndex == 11) {
+			JButton btn = new JButton("" + rowIndex);
+			return btn;
+		}
+		return null;
+	}
+	// da bismo mogli prikazati dugme
+	@Override
+	public Class<?> getColumnClass(int columnIndex) {
+		switch (columnIndex) {
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+			return JButton.class;
+		default:
+			return null;
+		}
+	}
+	@Override
+	public String getColumnName(int col) {
+		return columnNames[col];
+	}
+	@Override
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		return columnIndex == 11;
+	}
+
+}
