a456c4a16f67d4e09b8662a0111902a42bec1354
Sladjana <sladjasavkovic333@gmail.com>
2019-12-15 23:49:12 +0100

    #TabbedPane
    Implenetirana funkcionalnost prikazivanja dugmica u toolbar-u u zavisnosti na kojem se tabu nalazim.

--- StudentskaSluzba/src/view/TabbedPane.java
+++ StudentskaSluzba/src/view/TabbedPane.java
@@ -5,7 +5,12 @@ package view;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.*;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
 
 /**
  * @author Sladjana Savkovic
@@ -27,10 +32,41 @@ public class TabbedPane extends JTabbedPane{
 		this.setMnemonicAt(0, KeyEvent.VK_1);
 		
 		MyTab secondTab=new MyTab("Profesori");
+		secondTab.addAncestorListener(new AncestorListener() {
+			@Override
+			public void ancestorRemoved(AncestorEvent arg0) {
+				Toolbar.buttonAddProfessor.setVisible(false);
+			}
+			@Override
+			public void ancestorMoved(AncestorEvent arg0) {
+				// TODO Auto-generated method stub
+			}
+			@Override
+			public void ancestorAdded(AncestorEvent arg0) {
+				Toolbar.buttonAddProfessor.setVisible(true);
+			}
+		});
 		this.addTab("Profesori", secondTab);
 		this.setMnemonicAt(1, KeyEvent.VK_2);
 		
 		MyTab thirdTab=new MyTab("Predmeti");
+		thirdTab.addAncestorListener(new AncestorListener() {
+			@Override
+			public void ancestorRemoved(AncestorEvent event) {
+				Toolbar.buttonAddProfessor.setVisible(false);
+				Toolbar.buttonAddStudent.setVisible(false);
+			}
+			
+			@Override
+			public void ancestorMoved(AncestorEvent event) {}
+		
+			@Override
+			public void ancestorAdded(AncestorEvent event) {
+				Toolbar.buttonAddProfessor.setVisible(true);
+				Toolbar.buttonAddStudent.setVisible(true);
+				
+			}
+		});
 		this.addTab("Predmeti", thirdTab);
 		this.setMnemonicAt(2, KeyEvent.VK_3);
 		
