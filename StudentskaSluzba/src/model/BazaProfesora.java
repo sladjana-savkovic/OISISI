dd5ec813bdaba9e1606d0ce29ee4d6ce3a1dce83
Sladjana <sladjasavkovic333@gmail.com>
2019-12-16 13:36:50 +0100

    #BazaProfesora
    U bazi profesora dodani novi predmeti da bi se testirale funkcionalnosti spiska predmeta.

--- StudentskaSluzba/src/model/BazaProfesora.java
+++ StudentskaSluzba/src/model/BazaProfesora.java
@@ -23,6 +23,7 @@ public class BazaProfesora {
 	//samo da ubacim neke podatke za pocetak
 	private ArrayList<Profesor> profesori;
 	private ArrayList<String> predmeti;
+	private ArrayList<String> predmeti1;
 	
 	public BazaProfesora() {
 		initProfesore();
@@ -31,19 +32,26 @@ public class BazaProfesora {
 	public void initProfesore() {
 		this.profesori = new ArrayList<Profesor>();
 		this.predmeti = new ArrayList<String>();
+		this.predmeti1 = new ArrayList<String>();
 		predmeti.add("E2123");
 		predmeti.add("E3125");
 		predmeti.add("SIIT852");
+		predmeti1.add("E1042");
+		predmeti1.add("MA750");
+		predmeti1.add("II802");
 		profesori.add(new Profesor("Nebojša","Ralević","23.08.1965.","Ćirpanova 25","062/782-41-02","rale65@gmail.com",
 				"Trg Dositeja Obradovića 10","124kl789","doktor","redovni prof.",predmeti));
 		profesori.add(new Profesor("Milan","Rapaić","16.11.1981.","Narodnog fronta 105","062/581-00-72","rapaja1981@gmail.com",
-				"Trg Dositeja Obradovića 10","581af102","doktor","vanredni prof.",predmeti));
+				"Trg Dositeja Obradovića 10","581af102","doktor","vanredni prof.",predmeti1));
 	}
 	public ArrayList<Profesor> getProfesori() {
 		return profesori;
 	}
 	public void setProfesori(ArrayList<Profesor> profesori) {
 		this.profesori = profesori;
+	}
+	public Profesor getProfesorIndex(int index) {
+		return this.profesori.get(index);
 	}
 	public String getValueAt(int row, int column) {
 		Profesor profesor=this.profesori.get(row);
