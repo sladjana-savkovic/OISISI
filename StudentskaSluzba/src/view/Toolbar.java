b0f169460c3ab38668c82194a3b4fbbcfde71ac3
Sladjana <sladjasavkovic333@gmail.com>
2019-12-16 13:41:45 +0100

    #Toolbar

--- StudentskaSluzba/src/view/Toolbar.java
+++ StudentskaSluzba/src/view/Toolbar.java
@@ -13,11 +13,12 @@ import java.awt.event.KeyEvent;
  */
 public class Toolbar extends JToolBar{
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
+		
+	static JButton buttonAddStudent = new JButton();
+	static JButton buttonAddProfessor = new JButton();
+	
 	public Toolbar() {
 		// u konstruktor natklase, JToolbar prosledjujemo orijentaciju toolbar-a
 		super(SwingConstants.HORIZONTAL);
@@ -27,57 +28,64 @@ public class Toolbar extends JToolBar{
 		JButton buttonAdd = new JButton();
 		buttonAdd.setToolTipText("Dodaj(CTRL+A)");
 		buttonAdd.setIcon(new ImageIcon("logo_images/plus.jpg"));
-		buttonAdd.setMnemonic(KeyEvent.VK_A);
+		//buttonAdd.setMnemonic(KeyEvent.VK_A);
 		buttonAdd.setBackground(lightBlue);
 		add(buttonAdd);
 		
 		JButton buttonModify = new JButton();
 		buttonModify.setToolTipText("Izmijeni(CTRL+M)");
 		buttonModify.setIcon(new ImageIcon("logo_images/pencil.jpg"));
-		buttonAdd.setMnemonic(KeyEvent.VK_M);
+		//buttonAdd.setMnemonic(KeyEvent.VK_M);
 		buttonModify.setBackground(lightBlue);
 		add(buttonModify);
 		
 		JButton buttonDelete = new JButton();
 		buttonDelete.setToolTipText("Obrisi(CTRL+D)");
 		buttonDelete.setIcon(new ImageIcon("logo_images/delete.jpg"));
-		buttonAdd.setMnemonic(KeyEvent.VK_D);
+		//buttonAdd.setMnemonic(KeyEvent.VK_D);
 		buttonDelete.setBackground(lightBlue);
 		add(buttonDelete);
 		
-		JButton buttonAddStudent = new JButton();
+		//JButton buttonAddStudent = new JButton();
 		buttonAddStudent.setToolTipText("Dodaj studenta(CTRL+S)");
 		buttonAddStudent.setIcon(new ImageIcon("logo_images/add_student.jpg"));
-		buttonAdd.setMnemonic(KeyEvent.VK_S);
+		//buttonAdd.setMnemonic(KeyEvent.VK_S);
 		buttonAddStudent.setBackground(lightBlue);
 		add(buttonAddStudent);
-		buttonAddStudent.setVisible(true);
+		setVisibleButtonAddStudent(false);
 		
-		JButton buttonAddProfessor = new JButton();
+		//JButton buttonAddProfessor = new JButton();
 		buttonAddProfessor.setToolTipText("Dodaj profesora(CTRL+P)");
 		buttonAddProfessor.setIcon(new ImageIcon("logo_images/add_professor.jpg"));
-		buttonAdd.setMnemonic(KeyEvent.VK_P);
+		//buttonAdd.setMnemonic(KeyEvent.VK_P);
 		buttonAddProfessor.setBackground(lightBlue);
 		add(buttonAddProfessor);
-		buttonAddProfessor.setVisible(true);
+		setVisibleButtonAddProfessor(false);
 		
 		//Dodaje horizontalni razmak izmedju dugmica sa lijeve i desne strane
 		//add(Box.createHorizontalGlue());
 		add(Box.createHorizontalStrut(900));
 		
-		JPanel panelSearch=new JPanel();
+		//JPanel panelSearch=new JPanel();
 		JTextField textSearch=new JTextField();
 		textSearch.setPreferredSize(new Dimension(200,30));
 		textSearch.setFont(new Font("TimesRoman", Font.PLAIN, 18));
-		panelSearch.add(textSearch);
-		add(panelSearch);
+		//panelSearch.add(textSearch);
+		add(textSearch);
 		
 		JButton buttonSearch = new JButton();
 		buttonSearch.setToolTipText("Pretraži(CTRL+F)");
 		buttonSearch.setIcon(new ImageIcon("logo_images/search.jpg"));
-		buttonAdd.setMnemonic(KeyEvent.VK_F);
+		//buttonAdd.setMnemonic(KeyEvent.VK_F);
 		buttonSearch.setBackground(lightBlue);
 		add(buttonSearch);
 		buttonSearch.setVisible(true);
+	}
+	
+	static public void setVisibleButtonAddStudent(boolean flag){
+		buttonAddStudent.setVisible(flag);
+	}
+	static public void setVisibleButtonAddProfessor(boolean flag) {
+		buttonAddProfessor.setVisible(flag);
 	}
 }
