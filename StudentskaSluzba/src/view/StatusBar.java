e087ddb139b1f8bd467955983e447e3f7bd5004a
Dragana Carapic <dragana.carapic98@uns.ac.rs>
2019-12-16 20:37:27 +0100

    #StatusBar

--- StudentskaSluzba/src/view/StatusBar.java
+++ StudentskaSluzba/src/view/StatusBar.java
@@ -3,22 +3,43 @@
  */
 package view;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
 
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 /**
  * @author Dragana Carapic
  *
  */
-public class StatusBar extends JLabel{
+public class StatusBar extends JPanel{
 	private static final long serialVersionUID = 1L;
     
     public StatusBar() {
     	super();
-    	String vrijeme = new SimpleDateFormat("dd MMM yyyy hh:mm:ss").format(Calendar.getInstance().getTime());
-    	setText("Studentska sluzba"+"      "+vrijeme);
+    	JLabel naslov = new JLabel("Studentska sluzba", JLabel.LEFT);
+    	JLabel datum =  new JLabel();
+    	
+    	this.add(naslov, BorderLayout.WEST);
+        this.add(datum, BorderLayout.EAST);
+    	
+        
+        Timer t = new javax.swing.Timer(1000, new ActionListener() {
+        	
+        	public void actionPerformed(ActionEvent e) {
+                java.util.Date now = new java.util.Date();
+                String ss = DateFormat.getDateTimeInstance().format(now);
+                datum.setText(ss);
+                datum.setToolTipText(ss);
+ 
+            }
+        });
+        t.start();
     }
 
 	
