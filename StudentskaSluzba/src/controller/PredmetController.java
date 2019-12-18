f7d3acd78d88d16198734f55a79fa026db28c320
Sladjana <sladjasavkovic333@gmail.com>
2019-12-18 15:56:46 +0100

    #PredmetController

---
+++ StudentskaSluzba/src/controller/PredmetController.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package controller;
+
+import model.BazaPredmeta;
+import model.Predmet;
+
+/**
+ * @author Sladjana Savkovic
+ *
+ */
+public class PredmetController {
+	
+	private static PredmetController instance = null;
+	
+	public static PredmetController getInstance() {
+		if (instance == null) {
+			instance = new PredmetController();
+		}
+		return instance;
+	}
+	
+	private PredmetController() {}
+	
+	public void obrisiPredmet(int rowSelectedIndex) {
+    	if (rowSelectedIndex < 0) {
+			return;
+		}
+    	// izmena modela
+    	Predmet predmet = BazaPredmeta.getInstance().getPredmetIndex(rowSelectedIndex);
+    	BazaPredmeta.getInstance().obrisiPredmet(predmet.getSifra());
+		// azuriranje prikaza
+		
+    }
+}
