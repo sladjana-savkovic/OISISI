6db0fa638a33ab48cd444925b9c5818cfb721f0b
Dragana Carapic <dragana.carapic98@uns.ac.rs>
2019-12-18 12:33:30 +0100

    #Baza studenata

---
+++ StudentskaSluzba/src/model/BazaStudenata.java
@@ -0,0 +1,129 @@
+/**
+ * 
+ */
+package model;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import model.Student.statusStudenta;
+
+/**
+ * @author Dragana Carapic
+ *
+ */
+public class BazaStudenata implements Serializable{
+
+	private static final long serialVersionUID = -2220866782731925699L;
+	
+	private static BazaStudenata instance = null;
+	
+	public static BazaStudenata getInstance() {
+		if(instance==null) {
+			instance = new BazaStudenata();
+		}
+		return instance;
+	}
+	
+	private ArrayList<Student> studenti;
+	private ArrayList<String> predmeti1;
+	private ArrayList<String> predmeti2;
+	
+	public BazaStudenata() {
+		readStudenti();
+		initStudent();
+		writeStudenti();
+		
+	}
+	public void initStudent() {
+		this.studenti = new ArrayList<Student>();
+		this.predmeti1 = new ArrayList<String>();
+		this.predmeti2 = new ArrayList<String>();
+		
+		predmeti1.add("E2123");
+		predmeti1.add("E2485");
+		predmeti2.add("E2458");
+		predmeti2.add("E1042");
+		predmeti2.add("MA750");
+		predmeti2.add("II802");
+		
+		studenti.add(new Student("Ana","Mihić","13.05,1998.","Despota Stefana 8", "+38165987654", "anam@gmail.com", "RA1/2017","01.07.2017","3",9.42,statusStudenta.B,predmeti1));
+		studenti.add(new Student("Miloš","Lukić","31.12,1996.","Cara Dušana 91", "+38169981132", "milos996@gmail.com", "RA78/2015","03.07.2015","3",7.56,statusStudenta.S,predmeti1));
+		studenti.add(new Student("Tijana","Marić","18.10,1997.","Šekspirova 84", "+38162001882", "mtijana1997@gmail.com", "RA25/2016","10.09.2015","4",7.02,statusStudenta.B,predmeti1));
+		studenti.add(new Student("Igor","Trivalić","10.03,1998.","Cara Lazara 3", "+38163335789", "igort98@gmail.com", "PSI14/2017","10.07.2017","3",9.50,statusStudenta.B,predmeti2));
+		studenti.add(new Student("Aleksa","Tadić","11.08,1996.","Stražilovska 10", "+38165130065", "talek996@gmail.com", "E3154/2015","01.07.2015","3",6.98,statusStudenta.S,predmeti2));
+		studenti.add(new Student("Milica","Sarić","10.02,1996.","Maksima Gorkog 23", "+38163133099", "milicas96@gmail.com", "MEH215/2016","04.07.2016","4",8.76,statusStudenta.B,predmeti2));
+	
+	}
+	
+	public ArrayList<Student> getStudenti(){
+		return studenti;
+	}
+	
+	public void setStudenti(ArrayList<Student> studenti) {
+		this.studenti = studenti;
+	}
+	
+	public Student getStudentIndex(int index) {
+		return this.studenti.get(index);
+	}
+	
+	public String getValueAt(int row, int column) {
+		Student student=this.studenti.get(row);
+		switch (column) {
+		case 0:
+			return student.getIme();
+		case 1:
+			return student.getPrezime();
+		case 2:
+			return student.getDatumRodjenja();
+		case 3:
+			return student.getAdresaStanovanja();
+		case 4:
+			return student.getBrojTelefona();
+		case 5:
+			return student.getEmailAdresa();
+		case 6:
+			return student.getBrojIndeka();
+		case 7:
+			return student.getDatumUpisa();
+		case 8:
+			return student.getTrenutnaGodinaStudija();
+		case 9:
+			return Double.toString(student.getProsjecnaOcjena());
+		case 10:
+			return student.getStatus().name();
+		case 11:
+			return Integer.toString(student.getBrojPredmeta());
+		default:
+			return null;
+		}
+	}
+	
+	private void writeStudenti(){
+		try {
+			ObjectOutputStream out = new ObjectOutputStream((new FileOutputStream("data_files/studenti.dat")));
+			out.writeObject(studenti);
+		    out.close();
+		} catch (IOException e) {
+		      e.printStackTrace();
+	    }
+	}
+
+	private void readStudenti(){
+		try {
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream("data_files/studenti.dat"));
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		} 
+	}
+
+
+}
