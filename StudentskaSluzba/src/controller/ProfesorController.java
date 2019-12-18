7bc818421496cdd27d800f762805cbc1d062cddf
Sladjana <sladjasavkovic333@gmail.com>
2019-12-18 16:07:30 +0100

    #ProfesorController

---
+++ StudentskaSluzba/src/controller/ProfesorController.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package controller;
+
+import model.BazaProfesora;
+import model.Profesor;
+
+/**
+ * @author Sladjana Savkovic
+ *
+ */
+public class ProfesorController {
+	
+private static ProfesorController instance = null;
+	
+	public static ProfesorController getInstance() {
+		if (instance == null) {
+			instance = new ProfesorController();
+		}
+		return instance;
+	}
+	
+	private ProfesorController() {}
+	
+	public void obrisiProfesora(int rowSelectedIndex) {
+    	if (rowSelectedIndex < 0) {
+			return;
+		}
+    	// izmena modela
+    	Profesor profesor = BazaProfesora.getInstance().getProfesorIndex(rowSelectedIndex);
+    	BazaProfesora.getInstance().obrisiProfesora(profesor.getBrLicneKarte());
+		// azuriranje prikaza
+		
+    }
+}
