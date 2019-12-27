54970bd6750cb4c8fd8cfc42f2b24a72c8a5b51a
Sladjana <sladjasavkovic333@gmail.com>
2019-12-28 00:35:32 +0100

    #PretragaProfesora
    Implementirana pretraga na novi nacin

--- StudentskaSluzba/src/view/PretragaProfesora.java
+++ StudentskaSluzba/src/view/PretragaProfesora.java
@@ -4,10 +4,8 @@
 package view;
 
 import javax.swing.JOptionPane;
-
-import controller.ProfesorController;
-import model.BazaProfesora;
-import model.Profesor;
+import javax.swing.RowFilter;
+import javax.swing.table.TableRowSorter;
 
 /**
  * @author Sladjana Savkovic
@@ -15,113 +13,113 @@ import model.Profesor;
  */
 public class PretragaProfesora {
 	
-	String ime="";
-	String prezime="";
-	String datumRodjenja="";
-	String adresaStanovanje="";
-	String telefon="";
-	String email="";
-	String adresaKancelarije="";
-	String brLicneKarte="";
-	String titula="";
-	String zvanje="";
-	String[] brojeviLicne=new String[100]; //pamtim koji su brojevi licne karte profesora koji su nekad bili u bazi
-	int brojac=0;
+	String ime;
+	String prezime;
+	String datumRodjenja;
+	String adresaStanovanje;
+	String telefon;
+	String email;
+	String adresaKancelarije;
+	String brLicneKarte;
+	String titula;
+	String zvanje;
 	
-	public PretragaProfesora(String pretraga) {
-		if(pretraga.equals(""))
-			ResetPretrage();
+	public PretragaProfesora(String pretraga,TableRowSorter<AbstractTableModelProfesor> sorter) {
 		
-		boolean rezultatPretrage = pretrazi(pretraga);
-		if(rezultatPretrage == false && !pretraga.equals(""))
+		ime="";
+		prezime="";
+		datumRodjenja="";
+		adresaStanovanje="";
+		telefon="";
+		email="";
+		adresaKancelarije="";
+		brLicneKarte="";
+		titula="";
+		zvanje="";
+		
+		int rezultatPretrage = pretrazi(pretraga);
+		
+		if(rezultatPretrage == 0 && !pretraga.equals("")) {
 			JOptionPane.showMessageDialog(null, "Pogresan unos za pretragu!","Greška",JOptionPane.ERROR_MESSAGE);
+		}
+		else {
+			for(int i=0;i<rezultatPretrage;i++) {
+				if(!ime.equals(""))
+					sorter.setRowFilter(RowFilter.regexFilter(ime));
+				else if(!prezime.equals(""))
+					sorter.setRowFilter(RowFilter.regexFilter(prezime));
+				else if(!datumRodjenja.equals(""))
+					sorter.setRowFilter(RowFilter.regexFilter(datumRodjenja));
+				else if(!adresaStanovanje.equals(""))
+					sorter.setRowFilter(RowFilter.regexFilter(adresaStanovanje,3));
+				else if(!telefon.equals(""))
+					sorter.setRowFilter(RowFilter.regexFilter(telefon,4));
+				else if(!email.equals(""))
+					sorter.setRowFilter(RowFilter.regexFilter(email,5));
+				else if(!adresaKancelarije.equals(""))
+					sorter.setRowFilter(RowFilter.regexFilter(adresaKancelarije,6));
+				else if(!brLicneKarte.equals(""))
+					sorter.setRowFilter(RowFilter.regexFilter(brLicneKarte,7));
+				else if(!titula.equals(""))
+					sorter.setRowFilter(RowFilter.regexFilter(titula,8));
+				else if(!zvanje.equals(""))
+					sorter.setRowFilter(RowFilter.regexFilter(zvanje,9));
+			}
+		}
 		
-		modifikujBazuProfesora();
 	}
-	public boolean pretrazi(String pretraga) {
+	public int pretrazi(String pretraga) {
+		int brojac = 0;
 
 		String[] podaci=new String[10];
 		podaci=pretraga.split(";");
 			
 		for(int i=0;i<podaci.length;i++) {
+			
 			String[] pom = new String[2];
 			pom=podaci[i].split(":");
 				
-			if(pom[0].toUpperCase().equals("IME") && (pom.length > 1))
+			if(pom[0].toUpperCase().equals("IME") && (pom.length > 1)) {
 				ime=pom[1];
-			else if(pom[0].toUpperCase().equals("PREZIME") && (pom.length > 1))
+				brojac++;
+			}
+			else if(pom[0].toUpperCase().equals("PREZIME") && (pom.length > 1)) {
 				prezime=pom[1];
-			else if( (pom[0].toUpperCase().equals("DATUM ROĐENJA") || pom[0].toUpperCase().equals("DATUM RODJENJA")) && (pom.length > 1) )
+				brojac++;
+			}
+			else if( (pom[0].toUpperCase().equals("DATUM ROĐENJA") || pom[0].toUpperCase().equals("DATUM RODJENJA")) && (pom.length > 1) ) {
 				datumRodjenja=pom[1];
-			else if(pom[0].toUpperCase().equals("ADRESA STANOVANJA") && (pom.length > 1))
+				brojac++;
+			}
+			else if(pom[0].toUpperCase().equals("ADRESA STANOVANJA") && (pom.length > 1)) {
 				adresaStanovanje=pom[1];
-			else if(pom[0].toUpperCase().equals("KONTAKT TELEFON") && (pom.length > 1))
+				brojac++;
+			}
+			else if(pom[0].toUpperCase().equals("KONTAKT TELEFON") && (pom.length > 1)) {
 				telefon=pom[1];
-			else if(pom[0].toUpperCase().equals("E-MAIL ADRESA") && (pom.length > 1))
+				brojac++;
+			}
+			else if(pom[0].toUpperCase().equals("E-MAIL ADRESA") && (pom.length > 1)) {
 				email=pom[1];
-			else if(pom[0].toUpperCase().equals("ADRESA KANCELARIJE") && (pom.length > 1))
+				brojac++;
+			}
+			else if(pom[0].toUpperCase().equals("ADRESA KANCELARIJE") && (pom.length > 1)) {
 				adresaKancelarije=pom[1];
-			else if((pom[0].toUpperCase().equals("BROJ LIČNE KARTE") || pom[0].toUpperCase().equals("BROJ LICNE KARTE"))&& (pom.length > 1))
+				brojac++;
+			}
+			else if((pom[0].toUpperCase().equals("BROJ LIČNE KARTE") || pom[0].toUpperCase().equals("BROJ LICNE KARTE"))&& (pom.length > 1)) {
 				brLicneKarte=pom[1];
-			else if(pom[0].toUpperCase().equals("TITULA") && (pom.length > 1))
+				brojac++;
+			}
+			else if(pom[0].toUpperCase().equals("TITULA") && (pom.length > 1)) {
 				titula=pom[1];
-			else if(pom[0].toUpperCase().equals("ZVANJE") && (pom.length > 1))
-				zvanje=pom[1];
-			else {
-				return false;
-			}	
-		}
-		return true;
-	}
-	public void modifikujBazuProfesora() {
-		int size=BazaProfesora.getInstance().getProfesori().size(); //Pocetna duzina,prije izmjene
-		for(int i=0; i<size; i++) {
-			Profesor p = BazaProfesora.getInstance().getProfesorIndex(i);
-			boolean flag=true;
-			for(int j=0; j<5; j++) {
-				if(!ime.equals(p.getIme()) && !ime.equals("")) { 
-					flag=false;
-				}
-				if(!prezime.equals(p.getPrezime()) && !prezime.equals("")) {
-					flag=false;
-				}
-				if(datumRodjenja!=p.getDatumRodjenja() && !datumRodjenja.equals("")){
-					flag=false;
-				}
-				if(adresaStanovanje!=p.getAdresaStanovanja()&& !adresaStanovanje.equals("")) {
-					flag=false;
-				}
-				if(!telefon.equals(p.getTelefon()) && !telefon.equals("")) {
-					flag=false;
-				}
-				if(!email.equals(p.getEmail()) && !email.equals("")) { 
-					flag=false;
-				}
-				if(!adresaKancelarije.equals(p.getAdresaKancelarije()) && !adresaKancelarije.equals("")) {
-					flag=false;
-				}
-				if(brLicneKarte!=p.getBrLicneKarte() && !brLicneKarte.equals("")){
-					flag=false;
-				}
-				if(titula!=p.getTitula() && !titula.equals("")) {
-					flag=false;
-				}
-				if(!zvanje.equals(p.getZvanje()) && !zvanje.equals("")) {
-					flag=false;
-				}
+				brojac++;
 			}
-			//Ako je flag postao false, znaci da nam vrijednost iz baze predmeta ne treba u bazi, pa je smjestam u pomocnu listu
-			if(flag == false) {
-				brojeviLicne[brojac++]=p.getBrLicneKarte();
+			else if(pom[0].toUpperCase().equals("ZVANJE") && (pom.length > 1)) {
+				zvanje=pom[1];	
+				brojac++;
 			}
 		}
-		//Dodani predmet brisem iz baze ali ga trazim po sifri jer se indeksi mijenjaju usljed brisanja
-		for(int k=0; k<brojac; k++) {
-			int pozicija = BazaProfesora.getInstance().getIndexOfProfesor(brojeviLicne[k]);
-			ProfesorController.getInstance().obrisiProfesora(pozicija);
-		}
-	}
-	public void ResetPretrage() {
-		//BazaProfesora.getInstance().readProfesoriFrom("data_files/profesori_pom.dat");
+		return brojac;
 	}
 }
