6af02542a268fe4dcbc0f3310882f35c958b7e2e
Dragana Carapic <dragana.carapic98@uns.ac.rs>
2019-12-28 13:41:49 +0100

    Dodavanje metode za izmjenu studenata

--- StudentskaSluzba/src/controller/StudentController.java
+++ StudentskaSluzba/src/controller/StudentController.java
@@ -3,6 +3,8 @@
  */
 package controller;
 
+import java.util.ArrayList;
+
 import model.BazaStudenata;
 import model.Student;
 import view.MyTab;
@@ -39,6 +41,57 @@ public class StudentController {
 		}
     	MyTab.azurirajPrikaz();
 		return true;
+	}
+	
+	public Student vratiSelektovanogStudenta(int rowSelectedIndex) {
+		Student s =null;
+		if(rowSelectedIndex<0) {
+			return s;
+		}
+		s = BazaStudenata.getInstance().getStudentIndex(rowSelectedIndex);
+		return s;
+	}
+	
+	//izmjena
+	public boolean izmjeniStudenta(int rowSelectedIndex, Student student, Student t) {
+		if(rowSelectedIndex < 0) {
+			return false;
+		}
+		
+		if(student.getBrojIndeka().equals(t.getBrojIndeka())){
+			BazaStudenata.getInstance().izmjeniStudenta(student, t);
+			MyTab.azurirajPrikaz();
+			return true;
+		}else {
+			 //nova metosa koja provjerava da li postoji st sa tim br indeksa ako postoji(vrati true) ret, a ako ne opet pozovi izmjenu
+			if(BazaStudenata.getInstance().getStudent(t.getBrojIndeka())!=null) {
+				return false;
+			}else {
+				BazaStudenata.getInstance().izmjeniStudenta(student, t);
+				MyTab.azurirajPrikaz();
+				return true;
+			}
+		}
+	}
+	
+	public void obrisiPredmetStudenta(int tableSelectedIndex,int listSelectedIndex) {
+		Student student = BazaStudenata.getInstance().getStudentIndex(tableSelectedIndex);
+		
+		if(listSelectedIndex != -1) {
+			BazaStudenata.getInstance().obrisiPredmet(student.getBrojIndeka(), listSelectedIndex);
+		}else {
+			return;
+		}
+	}
+	
+	public ArrayList<String> predmetiStudenata(int row){
+		ArrayList<String> predmeti = new ArrayList<String>();
+			if(BazaStudenata.getInstance().spisakPredmetaStudenata(row) != null) {
+				predmeti = BazaStudenata.getInstance().spisakPredmetaStudenata(row);
+				
+				return predmeti;
+			}
+			return null;
 	}
 	
 }
