c3a237d44a75ee63ef9f844f142a0038168a4105
Dragana Carapic <dragana.carapic98@uns.ac.rs>
2019-12-30 15:56:26 +0100

    Izmjena dijaloga za izmjenu profesora

--- StudentskaSluzba/src/view/IzmjenaProfesora.java
+++ StudentskaSluzba/src/view/IzmjenaProfesora.java
@@ -295,7 +295,7 @@ public class IzmjenaProfesora extends JDialog{
 					}
 					
 					//samo unos slova dozvonjen
-					Pattern pattern1 = Pattern.compile("^\\p{Alpha}+$", Pattern.UNICODE_CHARACTER_CLASS);
+					Pattern pattern1 = Pattern.compile("[a-zA-Z \\u0160-\\u0161-\\u0106-\\u0107-\\u017d-\\u017e-\\u010c-\\u010d-\\u0110-\\u0111-\\u0020]*", Pattern.UNICODE_CHARACTER_CLASS);
 					if(!(pattern1.matcher(imeStr)).matches()) {
 						JOptionPane.showMessageDialog(IzmjenaProfesora.this, "Dozvoljen je unos samo slova za ime!", "Upozorenje", JOptionPane.INFORMATION_MESSAGE);
 						txtIme.requestFocus();
