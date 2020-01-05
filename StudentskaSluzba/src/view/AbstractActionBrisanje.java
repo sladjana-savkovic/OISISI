9bcf30b3b6fdcefcf2bdb70f49a2b3007e4fd0bb
Sladjana <sladjasavkovic333@gmail.com>
2020-01-05 18:50:27 +0100

    #AbstractActionBrisanje
    Poziv metoda koje ce nakog brisanja nekog predmeta obrisati taj predmet iz liste predmeta koje predaje profesor. Poziv metode koja ce nakon brisanja profesora, obrisati tog profesora i sa predmeta na kojem je predavao.

--- StudentskaSluzba/src/view/AbstractActionBrisanje.java
+++ StudentskaSluzba/src/view/AbstractActionBrisanje.java
@@ -33,6 +33,7 @@ public class AbstractActionBrisanje extends AbstractAction{
 	public void actionPerformed(ActionEvent arg0) {
 		Object[] options = {"Odustanak","Potvrda"};
 		ImageIcon icon = new ImageIcon("logo_images/garbage.jpg");
+				
 		if ((TabbedPane.activeTab == 0) && (ButtonColumnStudent.selectedRow != -1)) {
 			int input0 = JOptionPane.showOptionDialog(null, "Da li ste sigurni da želite da obrišete studenta?","Brisanje studenta",
 					JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,icon,options,options[1]);
@@ -43,13 +44,32 @@ public class AbstractActionBrisanje extends AbstractAction{
 			int input1 = JOptionPane.showOptionDialog(null, "Da li ste sigurni da želite da obrišete profesora?","Brisanje profesora",
 					JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,icon,options,options[1]);
 			if(input1 == JOptionPane.NO_OPTION) {
+				
+				//prvo se profesor ukloni sa svakog predmeta koji je predavao
+				PredmetController.getInstance().uklanjanjeProfesoraSaSvakogPredmeta(ButtonColumnProfesor.selectedRow);
+				//zatim se izbrise sam profesor
 				ProfesorController.getInstance().obrisiProfesora(ButtonColumnProfesor.selectedRow);
+				
+				ButtonColumnStudent.selectedRow = -1;
+				ButtonColumnProfesor.selectedRow = -1;
+				ButtonColumnPredmet.selectedRow = -1;
 			}
 		} else if ((TabbedPane.activeTab == 2) && (ButtonColumnPredmet.selectedRow != -1)) {
 			int input2 = JOptionPane.showOptionDialog(null, "Da li ste sigurni da želite da obrišete predmet?","Brisanje predmeta",
 					JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,icon,options,options[1]);
 			if(input2 == JOptionPane.NO_OPTION) {
+				
+				//ovdje treba da se prvo obrise taj predmet sa spiska predmeta profesora
+				ProfesorController.getInstance().uklanjanjePredmetaIzListe(ButtonColumnPredmet.selectedRow);
+				//zatim se brise sam predmet
 				PredmetController.getInstance().obrisiPredmet(ButtonColumnPredmet.selectedRow);
+				
+				//if(PredmetController.getInstance().provjeriVelicinuListe())
+					//ButtonColumnPredmet.selectedRow = -1;
+					
+				ButtonColumnStudent.selectedRow = -1;
+				ButtonColumnProfesor.selectedRow = -1;
+				ButtonColumnPredmet.selectedRow = -1;
 			}
 		} else if((ButtonColumnStudent.selectedRow == -1) || (ButtonColumnProfesor.selectedRow == -1) || (ButtonColumnProfesor.selectedRow == -1)){
 			JOptionPane.showMessageDialog(null, "Izaberite neki red u tabeli!","Greška",JOptionPane.ERROR_MESSAGE);
