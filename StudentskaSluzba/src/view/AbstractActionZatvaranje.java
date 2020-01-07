17da874126552a36c66f34a3e61a6eafbfda3783
Dragana Carapic <dragana.carapic98@uns.ac.rs>
2020-01-07 12:52:10 +0100

    Izmjena apstraktne akcije za zatvaranje

--- StudentskaSluzba/src/view/AbstractActionZatvaranje.java
+++ StudentskaSluzba/src/view/AbstractActionZatvaranje.java
@@ -6,6 +6,8 @@ package view;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.JOptionPane;
+import javax.swing.WindowConstants;
+
 import com.sun.glass.events.KeyEvent;
 
 /**
@@ -23,13 +25,18 @@ public class AbstractActionZatvaranje extends AbstractAction{
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		Object[] options = {"Da","Ne"};
+	Object[] options = {"Da","Ne"};
 		//ImageIcon icon = new ImageIcon("logo_images/g.jpg");
 		int input0 = JOptionPane.showOptionDialog(null, "Da li ste sigurni da \u017eelite da zatvorite aplikaciju?","Zatvaranje aplikacije",
 				JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[1]);
-		if(input0==JOptionPane.YES_OPTION) {
+		if(input0!=JOptionPane.YES_OPTION) {
+			MainFrame.getInstance().setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		}else {
 			MainFrame.getInstance().dispose();
+			/*BazaStudenata.getInstance().writeStudenti();
+			BazaPredmeta.getInstance().writePredmeti();
+			BazaProfesora.getInstance().writeProfesori();*/
+			System.exit(0);
 		}
-		
 	}
 }
