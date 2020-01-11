4db6949521988f2f4953f3be2ba3472f63c4c776
Dragana Carapic <dragana.carapic98@uns.ac.rs>
2020-01-11 17:31:00 +0100

    #HelpDialog

--- StudentskaSluzba/src/view/HelpDialog.java
+++ StudentskaSluzba/src/view/HelpDialog.java
@@ -41,19 +41,20 @@ public class HelpDialog extends JDialog{
 	public void init(JFrame parent) {
 		try {
 			Toolkit kit=Toolkit.getDefaultToolkit();
-			setSize(400,400);
+			setSize(450,500);
 			Image img=kit.getImage("logo_images/ftn.png");
 			setIconImage(img);
 			setLocationRelativeTo(parent);
+	
 			
 			JPanel panelCenter = new JPanel(new BorderLayout());
 			BoxLayout boxCenter = new BoxLayout(panelCenter, BoxLayout.Y_AXIS);
 			panelCenter.setLayout(boxCenter);
-			panelCenter.add(Box.createVerticalStrut(30));
+			//panelCenter.add(Box.createVerticalStrut(30));
 			panelCenter.add(Box.createGlue());
 			
 			Dimension dim = new Dimension(180, 20);
-			Dimension dim1 = new Dimension(350, 140);
+			Dimension dim1 = new Dimension(410, 160);
 			
 			JPanel panel1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
 			
@@ -63,21 +64,28 @@ public class HelpDialog extends JDialog{
 			
 			//JPanel p1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
 			a1= new JTextArea(
-					"Sla\u0111ana Savkovi\u0107 hhdh gggghdhdkkkkk je ro\u0111ena"+"\n"+
-					"20.01.1997. u Banja Luci, BiH, hhhhhhhhhhhhhhhhhh d dhh."+"\n"+
-					"Osnovnu \u0161kolu je zavrjjjjjjjjjjjjj  dffd f df dff dff."+"\n"+
-					"u Prnjavoru, nakon \u010dega je upisalajjjjjjjjjjjjjjjjjjjj"+"\n"+
-					"Gimanziju, ra\u010dunarski smjer istokkkkkkkkkkkkkkd djjjjj"+"\n"+
-					"u Prnjavoru.Po zavr\u0161etku gimnazijejjjjjjjjjjj djjjjjjj"+"\n"+
-					"Fakultet tehni\u010dkih nauka u Novomjjjjjjjjjdg dgjjjjjjjj"+"\n"+
-					"Sadu je upisala 2017.jjjjjjjjjjjjjjjjjjjjjjjjj  fd fd dfjjjj"
-					);
-			a1.setPreferredSize(dim1);
+					" Aplikacija prikazuje informacioni sistem studenstke slu\u017ebe i sadr\u017ei\n"
+					+ " opcije za rukovanje podacima tog sistema. Osnovne funkcionalnosti\n"
+					+ " aplikacije su dodavanje, izmjena i brisanje entiteta sistema.\n"+ 
+					" Postoje\u0107i entiteti se mogu sortirati  i pretra\u017eiti po odre\u0111enom\n"+
+					" kriterijumu. Entitete sistema \u010dine student, profesor i predmet.\n"+
+					" Svaki entitet sadr\u017ei svoju tabelu sa podacima nad kojima korisnik\n" +
+					" primjenjuje gore navedene funkcionalnosti.\n"+
+					" Glavni prozor aplikacije sadr\u017ei prikaz trenurnog vremena i datuma,\n"
+					+ " a tako\u0111e omogu\u0107ava korisniku izlaz iz aplikacije, minimizaciju i \n"
+					+ " maksimizaciju prozora.\n"+
+					" Biografije autora se nalaze u stavci menija pod nazivom \"O nama\".");
+			a1.setMaximumSize(dim1);
+			//a1.getPreferredScrollableViewportSize();
+			//a1.setMaximumSize(getPreferredSize());
 			a1.setBackground(getBackground());
 			a1.setEditable(false);
 			
 			JScrollPane scroll = new JScrollPane(a1);
+			scroll.setPreferredSize(dim1);
+			//scroll.setMaximumSize(dim1);
 	        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+	        //scroll.setMaximumSize(dim1);
 			
 			JPanel panel2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
 			text1 = new JLabel("Rukovanje aplikacijom");
@@ -87,21 +95,58 @@ public class HelpDialog extends JDialog{
 			
 			//JPanel p2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
 			a2= new JTextArea(
-					"Sla\u0111ana Savkovi\u0107 hhdh dhdhdhdhjjj je ro\u0111ena"+"\n"+
-					"20.01.1997. u Banja Luci,jjjjjjjjjjjjjjjjjj BiH, RS."+"\n"+
-					"Osnovnu \u0161kolu je zavr\u0161ila jjjjjjjjjjjjff 2011."+"\n"+
-					"u Prnjavoru, nakon \u010dega je upisala"+"\n"+
-					"Gimanziju, ra\u010dunarski smjer isto"+"\n"+
-					"u Prnjavoru.Po zavr\u0161etku gimnazije"+"\n"+
-					"Fakultet tehni\u010dkih nauka u Novom"+"\n"+
-					"Sadu je upisala 2017. "
+					" Glavni prozor aplikacije sadr\u017ei meni bar, toolbar, tabove sa\n"
+					+ " tabelama entiteta i status bar. Osnovnim funkcionalnostima\n "
+					+ " sistema korisnik moze da pristupi koriste\u0107i meni bar ili toolbar.\n" + 
+					" U zavisnosti koja tabela entiteta je trenutno vidljiva, osnovne\n "
+					+ " funkcionalnosti se pozivaju nad tom tabelom.\n"
+					+ " Pre\u010dice za prikaz dijaloga za dodavanje Studenta/Profesora/Predmeta\n"
+					+ " su ALT+D i CTRL+D, za izmjenu ALT+I i CTRL+I, za brisanje ALT+B i \n"
+					+ " CTRL+B. Pre\u010dice za odabir tabele koja ce biti vidljiva su: ALT+S za \n"
+					+ " studente, ALT+P za profesore i ALT+R za predmete.\n"
+					+ " Pre\u010dica za pretragu podataka nad tabelom je ALT+T.\n" + 
+					" Dijaloge \"Pomo\u0107\" i \"O nama\" je mogu\u0107e prikazati pomo\u0107u CTRL+P,\n "
+					+ " CTRL+O respektivno. Pre\u010dica za zatvaranje aplikacije je CTRL+Z.\n" + 
+					" Dodavanje entiteta u sistem \u0107e biti uspje\u0161no ako je korisnik validno\n"
+					+ " unio sva polja. Da bi izmijenili podatke nekog od entiteta, ili ga\n"
+					+ " obrisali neophodno je selektovati odgovaraju\u0107i red u tabeli. Nakon \n"
+					+ " uspje\u0161ne izmjene, novi podaci \u0107e biti prikazani u tabeli, kao i na\n"
+					+ " svim ostalim mjestima gdje se taj podatak prikazuje. Npr. ako \n"
+					+ " korisnik promjeni indeks studenta, ta promjena ce biti vidljiva i u\n "
+					+ " tabeli predmet na spisku studenata na kome se nalazi izmijenjeni \n"
+					+ " student. Po sli\u010dnom principu se obavlja i brisanje. \n" + 
+					" Pretraga podataka iz tabele se obavlja unosom naziva kolone po kojoj\n"
+					+ " pretra\u017eujemo i podatka u toj koloni. Mogu\u0107a je i vi\u0161estruka pretraga\n"
+					+ " po vi\u0161e od jedne kolone. Pretraga ce biti validna i ako korisnik ne\n"
+					+ " unese pun naziv podatka iz kolone. Npr. pretragu predmeta je mogu\u0107e \n"
+					+ " obaviti unosom \u0160ifra:OP301;Naziv:Osnovne. Obavezan je unos\n "
+					+ " delimitera \":\" izme\u0111u naziva kolone i podatka i delimitera \";\" \n"
+					+ " ako je pretraga vi\u0161estruka. (Napomena: unos Profesor:, je validan i \n"
+					+ " prikazuje spisak svih predmeta koji imaju profesora).Sortiranje \n"
+					+ " podataka u tabeli se vr\u0161i klikom na naziv kolone koju \u017eelimo sortirati.\n" + 
+					" Odabirom tabele Predmeti u toolbar-u  se prikazuju nova tri dugmeta za\n"
+					+ " funkcionalnosti: dodavanje studenta na predmet, dodavanje profesora\n"
+					+ " na predmet i brisanje profesora sa predmeta. Za sve tri opcije\n "
+					+ " neophodno je selektovati odgovaraju\u0107i red u tabeli. Uslovi za \n"
+					+ " dodavanje studenta na predmet su da je student odgovaraju\u0107a\n" + 
+					" godina studija i da ve\u0107 ne slu\u0161a taj predmet. Uslov za dodavanje\n"
+					+ " profesora na predmet je da profesor ne predaje taj predmet. Uslov za\n"
+					+ " brisanje profesora sa predmeta je da postoji profesor na tom \n"
+					+ " predmetu. Ove tri funkcionalnosti uti\u010du na izmjenu podataka u\n"
+					+ " preostale dvije tabele."
 					);
-			a2.setPreferredSize(dim1);
+			a2.setMaximumSize(dim1);
+			//a2.setMaximumSize(getPreferredSize());
+			a2.getPreferredScrollableViewportSize();
 			a2.setBackground(getBackground());
 			a2.setEditable(false);
 			
 			JScrollPane scroll1 = new JScrollPane(a2);
+			scroll1.setPreferredSize(dim1);
+			//scroll1.getVerticalScrollBar().setUnitIncrement(10);
+			//scroll1.setMaximumSize(dim1);
 	        scroll1.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+	        //scroll1.setMaximumSize(dim1);
 		
 			panelCenter.add(panel1);
 			panelCenter.add(scroll);
